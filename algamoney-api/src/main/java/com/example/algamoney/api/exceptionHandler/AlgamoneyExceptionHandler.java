1b6ddc0e74cfbfcd75a7a1568eb7d4d0f6dbf375
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-02-07 07:56:33 -0300

    Implementando atualização parcial com PUT

--- algamoney-api/src/main/java/com/example/algamoney/api/exceptionHandler/AlgamoneyExceptionHandler.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/exceptionHandler/AlgamoneyExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.algamoney.api.exceptionHandler;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -31,8 +32,9 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
 		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
-		String mensagemDesenvolvedor = ex.getCause().toString();
-		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+		String mensagemDesenvolvedor = Optional.ofNullable(ex.getCause()).orElse(ex).toString();
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
 
 		
