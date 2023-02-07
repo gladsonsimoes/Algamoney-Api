da2620f923c153e043e482b46e3e5e6dcddfa5f8
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-02-07 09:35:15 -0300

    Criando a migração e entidade de lançamento

--- algamoney-api/src/main/java/com/example/algamoney/api/exceptionHandler/AlgamoneyExceptionHandler.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/exceptionHandler/AlgamoneyExceptionHandler.java
@@ -3,7 +3,6 @@ package com.example.algamoney.api.exceptionHandler;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -32,7 +31,7 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
 		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
-		String mensagemDesenvolvedor = Optional.ofNullable(ex.getCause()).orElse(ex).toString();
+		String mensagemDesenvolvedor = ex.getCause() != null ? ex.getCause().toString() : ex.toString();
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
